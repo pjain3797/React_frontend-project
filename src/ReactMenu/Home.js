c0448d03ef02c627fb0ca52544c644db1d781b91
Piyush <pjain3797@gmail.com>
2021-03-20 00:33:11 +0530

    minor xhanges

--- src/ReactMenu/Home.js
+++ src/ReactMenu/Home.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import web from "../images/img2.svg"
-import Common from './Common';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import web from "../images/img2.svg";
+import Common from "./Common";
 
 const Home = () => {
-return(
+  return (
     <>
-        <Common
-         name='Grow your business with' 
-         imgsrc={web} 
-         visit='/service' 
-         btname='Get Started' 
-     />
-        />
+      <Common
+        name="Grow your business with"
+        imgsrc={web}
+        visit="/service"
+        btname="Get Started"
+      />
     </>
-);
+  );
 };
 
 export default Home;
